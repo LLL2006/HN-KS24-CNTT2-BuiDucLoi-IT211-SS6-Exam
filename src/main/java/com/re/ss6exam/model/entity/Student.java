cfb023fb7d8ebd3641a8312fb8638b37af2fdf19
LLL <Zeikezan12345@gmail.com>
2026-05-20 15:07:30 +0700

    update

--- src/main/java/com/re/ss6exam/model/entity/Student.java
+++ src/main/java/com/re/ss6exam/model/entity/Student.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String fullName;
     private String email;
